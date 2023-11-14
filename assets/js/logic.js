85e9e9d7a37ba28a0289e8de4cc0a36225753973
VenkataMora <skreddy1719@gmail.com>
2023-11-14 00:10:27 +0000

    updated comments to the js files

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,15 +1,21 @@
+// Load hi scores from local storage
 document.addEventListener('DOMContentLoaded', function() {
+  // Retrieve high scores from local storage or use an empty array if there are no high scores
   var hiScore = JSON.parse(localStorage.getItem('highestScores')) || [];
+  // Get reference to the hi scores list
   var hiscoresList = document.getElementById('highscores');
+  // Function to clear hi scores
 function clearHiScores() {
   localStorage.removeItem('highestScores');
   hiscoresList.innerHTML = '';
 };
+ // Render hi scores to the list
 hiScore.forEach(score => {
   var listItem = document.createElement('li'); 
   listItem.textContent = `${score.intials}: ${score.score}`; 
   hiscoresList.appendChild(listItem); 
 });
+// Attach event listener to the "Clear Highscores" button
 var clearScoresButton = document.getElementById('clear'); 
 clearScoresButton.addEventListener('click', clearHiScores); 
 });
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,3 +1,4 @@
+// creates array with questions and answers
 var questionAnswers = [
     {
         ques: "Javascript is an _______ language?",
@@ -31,6 +32,7 @@ var questionAnswers = [
         ]
     }
 ];
+// Declaring DOM methods
 var startEl=document.getElementById('start');
 var timerEl=document.querySelector("#time");
 var startScreen=document.getElementById("start-screen");
@@ -41,91 +43,139 @@ var end = document.getElementById("end-screen");
 var finalScoreEl = document.getElementById("final-score");
 var intialsinput=document.querySelector("#initials");
 var submitEl=document.getElementById("submit");
-
+// timing and scoring variables
 var timer;
 var score = 0;
 var timerCount;
 var currentQuestion=0;
+// function to start the quiz
 startEl.addEventListener("click",function(event){
 	event.preventDefault();
+    // hide the start screen and display the question element
     startScreen.classList.add('hide');
     questionEl.classList.remove("hide"); 
+    // set the timer count based on the number of questions
 	timerCount=questionAnswers.length*15;
+    // start the timer and update the time displayed on the page
 	startScreen.classList.add('start');
 	stratime();
+    // get the questions and update the displayed question
 	getquestion();
 	
 });
+// function to start time
 function stratime(){
+    // Set timer countdown interval
 	timer=setInterval(function(){
-		timerCount--;
+		// Decrement timer count
+        timerCount--;
+        // Update timer element
 		timerEl.textContent=timerCount;
-		if(timerCount===0){
-			endGame();
+		// Check if timer has reached 0
+        if(timerCount===0){
+			// call end game function
+            endGame();
 		}
 	},1000);
 };
 
 
 
+// function to get question
 function getquestion(){
-var current = questionAnswers[currentQuestion];
-document.getElementById("question-title").textContent= current.ques;
-optionEls.innerHTML="";
-current.options.forEach(function(choice){
-    var button = document.createElement("button");
-    button.textContent = choice;
-    button.classList.add("choices");
-    button.addEventListener("click", correctAnswer);
-    optionEls.appendChild(button);
-});
+    // get current question object
+    var current = questionAnswers[currentQuestion];
+    
+    // set question title text
+    document.getElementById("question-title").textContent= current.ques;
+    
+    // clear option elements
+    optionEls.innerHTML="";
+    // iterate through current question options
+    current.options.forEach(function(choice){
+        // create button for each option
+        var button = document.createElement("button");
+        button.textContent = choice;
+        button.classList.add("choices");
+        
+        // add click event listener to button
+        button.addEventListener("click", correctAnswer);
+        
+        // append button to option elements
+        optionEls.appendChild(button);
+    });
 }
 
+// Checks the answer for the current question
 function correctAnswer(event){
+    // Get selected option
 	var selectedOption = event.target;
+    // Check if the selected option is correct
     var correct = selectedOption.textContent === questionAnswers[currentQuestion].answer;
+    
+    // Update score, feedback based on correctness
     if (correct) {
         score+=10;
         feedbackEl.textContent ="Correct!";
     } else{
         feedbackEl.textContent ="Wrong";
-    	timerCount =timerCount- 10;
+        timerCount =timerCount- 10;
     }
+    
+    // Show and hide feedback after 300ms
     feedbackEl.classList.remove("hide");
     setTimeout(() => {
         feedbackEl.classList.add("hide");
       }, 300);
-      currentQuestion++;
+    
+    // Move to the next question
+    currentQuestion++;
+    
+    // If there are more questions, get the next question
     if(currentQuestion<questionAnswers.length){
     	getquestion();
     }else{
         endGame();
     }
-};
-
+}
+// function to end the game
 function endGame() {  
+    // stop the timer
     clearInterval(timer);
+    // hide the question element
     questionEl.classList.add("hide");
     questionEl.style.display='none';
+     // display the end screen
     end.classList.remove("hide");
+    // show the final score
     finalScoreEl.textContent = score;
 };
-
+// function to highScore
 function highScores(){
+     // Check initials input and trim whitespaces
     var intials=intialsinput.value.trim();
+    // If initials not empty
     if(intials!==''){
+        // Create new high score object
         var newHighscore={intials,score};
+        // Retrieve existing high scores from local storage or initialize as empty array
         var highestScores=JSON.parse(localStorage.getItem("highestScores"))||[];
+        // Add new high score to the array
         highestScores.push(newHighscore);
+        // Store updated high scores in local storage
         localStorage.setItem('highestScores', JSON.stringify(highestScores));
+       // Redirect to high scores page
         window.location.href="highscores.html";
     }
 };
-
+// Event listener for starting the quiz
 startEl.addEventListener("click", getquestion);
-submitEl.addEventListener("click", highScores);
 var highScoresLink = document.querySelector(".scores a");
 highScoresLink.addEventListener("click", function(e){
+     // prevent default action
     e.preventDefault();
+     // navigate to highscores page
     window.location.href="highscores.html";
     });
+    // Event listener for submitting high score
+submitEl.addEventListener("click", highScores);
